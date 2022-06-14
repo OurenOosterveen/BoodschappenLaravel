2e975cc7b108bbe8b0680b903e90d4ea3deb9554
Methox <68101885+Methox@users.noreply.github.com>
2022-06-14 14:12:25 +0200

    Update Grocery.php
    Kleine opmerking

--- app/Models/Grocery.php
+++ app/Models/Grocery.php
@@ -11,7 +11,7 @@ class Grocery extends Model
 
     // protected $guarded = ['id']; mass assign all but - [] for no mass assign
     protected $fillable = ['name', 'number', 'price', 'category_id']; // App\Models\Grocery::create(['name' => 'ketchup', 'number' => 1, 'price' => 0.59]);
-
+    // TODO :: Dit zou een belongsto moeten zijn 
     public function categories() {
         return $this->hasOne(Category::class);
     }
